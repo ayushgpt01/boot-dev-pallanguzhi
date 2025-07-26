7152eb3ab4e14616e8411880b6b4a7ab2e4ec228
M-Sanjay12o52o <msnjy12o52o@gmail.com>
2025-07-26 23:15:03 +0530

    moved all files from js to ts

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,22 @@
-import { initApp } from "./pixi/initApp.js";
-import { createBoard } from "./pixi/board.js";
-import { createSeeds } from "./pixi/seeds.js";
-import { setupSeedInteractivity } from "./pixi/interactivity.js";
-import { createTitleText } from "./pixi/textBanner.js";
-import { Assets, TilingSprite, Sprite } from "pixi.js";
+import { initApp } from './pixi/initApp';
+import { createBoard } from './pixi/board.js';
+import { createSeeds } from './pixi/seeds.js';
+import { setupSeedInteractivity } from './pixi/interactivity.js';
+import { createTitleText } from './pixi/textBanner.js';
+import { Assets, TilingSprite, Sprite } from 'pixi.js';
 
 (async () => {
   const app = await initApp();
-  await Assets.init({ manifest: "/manifest.json" });
-  const seedAssets = await Assets.loadBundle("seeds");
+  await Assets.init({ manifest: '/manifest.json' });
+  const seedAssets = await Assets.loadBundle('seeds');
 
-  const texture = await Assets.load("/images/backgrounds/background_one.webp");
+  const texture = await Assets.load('/images/backgrounds/background_one.webp');
 
   const bgSprite = new Sprite({
     texture,
     width: app.screen.width,
-    height: app.screen.height,
+    height: app.screen.height
+  });
 
   app.stage.addChild(bgSprite);
 
