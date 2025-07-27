e006e017c0e4769f53d0a007852aad2e9b1cdfda
Harman926 <harmanjeetkaur126@gmail.com>
2025-07-27 09:23:29 +0530

    UI complete

--- src/main.js
+++ src/main.js
@@ -1,35 +1,26 @@
 import { Application, Assets, Sprite } from "pixi.js";
 
 (async () => {
-  // Create a new application
+  // Create Pixi application
   const app = new Application();
+  await app.init({
+    width: 400,
+    height: 300,
+    background: "#1099bb",
+  });
 
-  // Initialize the application
-  await app.init({ background: "#1099bb", resizeTo: window });
-
-  // Append the application canvas to the document body
+  // Add canvas to container
   document.getElementById("pixi-container").appendChild(app.canvas);
 
-  // Load the bunny texture
+  // Load texture
   const texture = await Assets.load("/assets/bunny.png");
-
-  // Create a bunny Sprite
   const bunny = new Sprite(texture);
-
-  // Center the sprite's anchor point
   bunny.anchor.set(0.5);
-
-  // Move the sprite to the center of the screen
   bunny.position.set(app.screen.width / 2, app.screen.height / 2);
-
-  // Add the bunny to the stage
   app.stage.addChild(bunny);
 
-  // Listen for animate update
+  // Rotate bunny
   app.ticker.add((time) => {
-    // Just for fun, let's rotate mr rabbit a little.
-    // * Delta is 1 if running at 100% performance *
-    // * Creates frame-independent transformation *
     bunny.rotation += 0.1 * time.deltaTime;
   });
 })();
