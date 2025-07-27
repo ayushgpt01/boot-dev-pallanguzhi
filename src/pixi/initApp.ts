c703f52cbaf4d326e91e4324ec5bf4fc60f339ed
Harman926 <harmanjeetkaur126@gmail.com>
2025-07-27 15:28:34 +0530

    sizingBOard

--- src/pixi/initApp.ts
+++ src/pixi/initApp.ts
@@ -2,7 +2,11 @@ import { Application } from 'pixi.js';
 
 export async function initApp() {
   const app = new Application();
-  await app.init({ background: '#4E342E', resizeTo: window });
+  await app.init({
+    background: '#4E342E',
+    width: 1000,   // set width
+    height: 500   // set height
+  });
   const container = document.getElementById('pixi-container');
   if (container) {
     container.appendChild(app.canvas);
