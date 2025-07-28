c7e7f1713a0412cf14e95a37f3d2fc85a06f84cf
Ayush Gupta <77984492+ayushgpt01@users.noreply.github.com>
2025-07-28 19:54:12 +0530

    add vite setup

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ayushgpt01/boot-dev-pallanguzhi', 
   plugins: [tailwindcss()],
   server: {
     port: 8080,
