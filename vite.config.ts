1405a2c048e4244fe318434cf058219af7376b92
ayushgpt01 <ayushgpt043@gmail.com>
2025-07-28 21:21:16 +0530

    fixed build issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/boot-dev-pallanguzhi',
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        game: resolve(__dirname, 'game.html'),
+        howto: resolve(__dirname, 'how-to.html')
+      }
+    }
+  },
   server: {
     port: 8080,
     open: true
